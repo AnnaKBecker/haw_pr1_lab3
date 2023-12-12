83262aca4c4d26479ce60eb7b709e035836de6f2
anna becker <150654806+AnnaKBecker@users.noreply.github.com>
2023-12-12 10:40:59 +0100

    Add files via upload
    Playing field without anything for tic-tac-toe

---
+++ spielfeld.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void){
+
+
+ 
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n");
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n");
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n");
+
+    printf("     |     |     \n\n");
+
+    return 0;
+}
